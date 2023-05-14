473084d13242de86f8be3d63fc4627762ae1d464
skocalar <sskocalar@gmail.com>
2023-05-14 01:04:08 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can use JavaScript to dynamically change the source or position of the overlay image
+// For example, you can use the following code to move the image up or down when a button is clicked:
+var overlayImage = document.getElementById("overlay-image");
+overlayImage.style.transform = "translateY(-50px)"; // move the image up by 50 pixels
